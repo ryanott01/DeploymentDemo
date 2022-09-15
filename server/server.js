00309955ce188a24f878316b84c5d60d9aa26e60
Ryan Ott <rott9501@gmail.com>
2022-09-14 23:34:47 -0700

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/style", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/style.css"))
+app.get("/styles", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/index.css"))
 })
 
 const port = process.env.PORT || 4005;
