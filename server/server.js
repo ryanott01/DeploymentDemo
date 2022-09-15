f1b2abfed4d60cc0eb701b005591e87db80a8c70
Ryan Ott <rott9501@gmail.com>
2022-09-14 23:29:20 -0700

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/styles", function(req, res){
+app.get("/style", function(req, res){
     res.sendFile(path.join(__dirname, "../public/style.css"))
 })
 
