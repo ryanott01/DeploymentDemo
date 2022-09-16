e68e2c0ea2abaedd6a34bf393f5eda33751e3e80
Ryan Ott <rott9501@gmail.com>
2022-09-15 19:00:12 -0700

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -8,12 +8,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public"));
+});
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/styles", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.css"));
 });
+
+
+
 
 const port = process.env.PORT || 4005;
 
