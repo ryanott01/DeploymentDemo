3b0d4dd07a52d3758768dae8590e871f7a0947bf
Ryan Ott <rott9501@gmail.com>
2022-09-15 19:25:56 -0700

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/pictures"));
+    res.sendFile(path.join(__dirname, "../public"));
 });
 
 app.get("/", function(req, res) {
